d7a8d9ecd0db37915a41a5184d6138536c46228d
Nethernumb <masondshaw06@gmail.com>
2025-04-24 15:27:56 -0600

    fixed the gui fix

--- guiprojfix/proj4_updated.py
+++ guiprojfix/proj4_updated.py
@@ -1,6 +1,10 @@
 #Mason Shaw
 #Project 4 - GUI Project
 
+# images won't load natively(?) you need to right click the folder that has the program, so in this case guiprojfix and click 
+# open in integrated terminal. in the terminal it opens you then type python then tab and it should autofill. if not put the file location. 
+
+# in my current directory i need to add guiprojfix to the beginning of all the image links. Without it it cant find anything. 
 
 import random
 from tkinter import *
@@ -11,58 +15,58 @@ from PIL import ImageTk, Image
 #                       DICTIONARIES FOR EACH HERO                           #
 #----------------------------------------------------------------------------#
 tank_heroes = {
-    1: ("D.VA", "images/dva.png"),
-    2: ("DoomFist", "images/doomfist.png"),
-    3: ("Hazard", "images/hazard.png"),
-    4: ("Junker Queen", "images/junkerqueen.png"),
-    5: ("Mauga", "images/mauga.png"),
-    6: ("Orisa", "images/orisa.png"),
-    7: ("Ramattra", "images/ramattra.png"),
-    8: ("Reinhardt", "images/reinhardt.png"),
-    9: ("Roadhog", "images/roadhog.png"),
-    10: ("Sigma", "images/sigma.png"),
-    11: ("Winston", "images/winston.png"),
-    12: ("Wrecking Ball", "images/wreckingball.png"),
-    13: ("Zarya", "images/zarya.png")
+    1: ("D.VA", "guiprojfix/images/dva.png"),
+    2: ("DoomFist", "guiprojfix/images/doomfist.png"),
+    3: ("Hazard", "guiprojfix/images/hazard.png"),
+    4: ("Junker Queen", "guiprojfix/images/junkerqueen.png"),
+    5: ("Mauga", "guiprojfix/images/mauga.png"),
+    6: ("Orisa", "guiprojfix/images/orisa.png"),
+    7: ("Ramattra", "guiprojfix/images/ramattra.png"),
+    8: ("Reinhardt", "guiprojfix/images/reinhardt.png"),
+    9: ("Roadhog", "guiprojfix/images/roadhog.png"),
+    10: ("Sigma", "guiprojfix/images/sigma.png"),
+    11: ("Winston", "guiprojfix/images/winston.png"),
+    12: ("Wrecking Ball", "guiprojfix/images/wreckingball.png"),
+    13: ("Zarya", "guiprojfix/images/zarya.png")
 }
 dps_heroes = {
-    1: ("Ashe", "images/ashe.png"),
-    2: ("Bastion", "images/bastion.png"),
-    3: ("McCree", "images/mccree.png"),
-    4: ("Echo", "images/echo.png"),
-    5: ("Freyja", "images/freyja.png"),
-    6: ("Genji", "images/genji.png"),
-    7: ("Hanzo", "images/hanzo.png"),
-    8: ("Junkrat", "images/junkrat.png"),
-    9: ("Mei", "images/mei.png"),
-    10: ("Pharah", "images/pharah.png"),
-    11: ("Reaper", "images/reaper.png"),
-    12: ("Sojourn", "images/sojourn.png"),
-    13: ("Soldier: 76", "images/soldier76.png"),
-    14: ("Sombra", "images/sombra.png"),
-    15: ("Symmetra", "images/symmetra.png"),
-    16: ("Torbin Time", "images/torbjorn.png"),
-    17: ("Tracer", "images/tracer.png"),
-    18: ("Venture", "images/venture.png"),
-    19: ("Widowmaker", "images/widowmaker.png")
+    1: ("Ashe", "guiprojfix/images/ashe.png"),
+    2: ("Bastion", "guiprojfix/images/bastion.png"),
+    3: ("McCree", "guiprojfix/images/mccree.png"),
+    4: ("Echo", "guiprojfix/images/echo.png"),
+    5: ("Freyja", "guiprojfix/images/freyja.png"),
+    6: ("Genji", "guiprojfix/images/genji.png"),
+    7: ("Hanzo", "guiprojfix/images/hanzo.png"),
+    8: ("Junkrat", "guiprojfix/images/junkrat.png"),
+    9: ("Mei", "guiprojfix/images/mei.png"),
+    10: ("Pharah", "guiprojfix/images/pharah.png"),
+    11: ("Reaper", "guiprojfix/images/reaper.png"),
+    12: ("Sojourn", "guiprojfix/images/sojourn.png"),
+    13: ("Soldier: 76", "guiprojfix/images/soldier76.png"),
+    14: ("Sombra", "guiprojfix/images/sombra.png"),
+    15: ("Symmetra", "guiprojfix/images/symmetra.png"),
+    16: ("Torbin Time", "guiprojfix/images/torbjorn.png"),
+    17: ("Tracer", "guiprojfix/images/tracer.png"),
+    18: ("Venture", "guiprojfix/images/venture.png"),
+    19: ("Widowmaker", "guiprojfix/images/widowmaker.png")
 }
 supp_heroes = {
-    1: ("Ana", "images/ana.png"),
-    2: ("Baptiste", "images/baptiste.png"),
-    3: ("Brigitte", "images/brigitte.png"),
-    4: ("Illari", "images/illari.png"),
-    5: ("Juno", "images/juno.png"),
-    6: ("Kiriko", "images/kiriko.png"),
-    7: ("Lifeweaver", "images/lifeweaver.png"),
-    8: ("Lucio", "images/lucio.png"),
-    9: ("Mercy", "images/mercy.png"),
-    10: ("Moira", "images/moira.png"),
-    11: ("Zenyatta", "images/zenyatta.png")
+    1: ("Ana", "guiprojfix/images/ana.png"),
+    2: ("Baptiste", "guiprojfix/images/baptiste.png"),
+    3: ("Brigitte", "guiprojfix/images/brigitte.png"),
+    4: ("Illari", "guiprojfix/images/illari.png"),
+    5: ("Juno", "guiprojfix/images/juno.png"),
+    6: ("Kiriko", "guiprojfix/images/kiriko.png"),
+    7: ("Lifeweaver", "guiprojfix/images/lifeweaver.png"),
+    8: ("Lucio", "guiprojfix/images/lucio.png"),
+    9: ("Mercy", "guiprojfix/images/mercy.png"),
+    10: ("Moira", "guiprojfix/images/moira.png"),
+    11: ("Zenyatta", "guiprojfix/images/zenyatta.png")
 }
 
 def tank_rand():
     index = random.randint(1, len(tank_heroes))
-    name, image_path = tank_heroes[1]
+    name, image_path = tank_heroes[index]
     hero_text.config(text=f"Your selected hero is {name}")
     img = ImageTk.PhotoImage(Image.open(image_path)) 
     #this is the old code that didn't work with png and jpg
@@ -80,7 +84,7 @@ def dps_rand():
 
 def supp_rand():
     index = random.randint(1, len(supp_heroes))
-    name, image_path = supp_heroes[11]
+    name, image_path = supp_heroes[index]
     hero_text.config(text=f"Your selected hero is {name}")
     img = ImageTk.PhotoImage(Image.open(image_path)) 
     hero_image_label.config(image=img)
