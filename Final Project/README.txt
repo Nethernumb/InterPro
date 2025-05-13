6724877dc41607a6ad584e925361e0aa5febb367
Nethernumb <masondshaw06@gmail.com>
2025-05-13 03:34:09 -0600

    made readme, added documentation and polished up old code in final

---
+++ Final Project/README.txt
@@ -0,0 +1,24 @@
+
+Mason Shaw
+Final Project
+
+This is my final project, it is supposed to be a clone of the web game adarkroom. 
+
+To be quite frank, I don't think I did that great of a job. I overestimated what I could do with the little time I had
+to juggle between so many other things. So as a result I plan on working on it when I can within the next few days even if it doesn't count 
+towards grading. 
+
+I plan on adding actual random events, and a more indepth system that involves crafting with the items you get, and just more base components
+of the actual game. I would also like to work on making it an executable file, since I think that would be pretty cool. The left side coloum could
+use some work, since it's pretty dry of information.
+
+It works fine and shouldn't have anything overlapping or breaking the program. It just lacks some more stuff inside of it in my opinion.
+Nothing should have to be installed either, since everything used should be built into pythons import system.
+
+My purpose was just for fun, since this is kind of a thing that's stuck with me since highschool, and it goes way more in depth than you would
+think it does. And I though it would make a good final project too.
+
+Tom helped me out with the most difficult chunks, like the time issue I had for timers, and formatting issues relating to frames
+and automatic formatting for them. He also gave me alot of information relating to quiestions I had and how to go about some things, so thank you Tom.
+
+Kevin was also a great help, giving me info and advice along the way.
--- Final Project/finalproj.py
+++ Final Project/finalproj.py
@@ -2,12 +2,13 @@
 #Intermediate Programming
 # Final Project
 
-
+#This is my final project. This one has been fixed and revised overtime with more to come within a day or so. The test file is a back up,
+# and more of a raw copy of this before some things commented out were removed, and before documentation on the project.
+# It is very slow, and pretty routine throughout. I would recommend tweaking the wait settings a bit if you are wanting to test it. I will mark them with a !!!
 
 import random
 import time
 from tkinter import *
-from tkinter import messagebox
 
 #---------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------VARIABLES-------------------------------------------------
@@ -17,7 +18,7 @@ counter = 0
 buttontime = 0
 
 wood = 10
-traps = 0
+traps = 0                   #These are my variables, everywhere they are references are globals, but that works out since I kind of need them all to be accessed by different functions
 fur = 0
 meat = 0
 teeth = 0
@@ -28,12 +29,12 @@ huts = 0
 #---------------------------------------------------------------------------------------------------------------
 
 def woodclicker():
-    global counter  
+    global counter              #Woodclicker function is the main function that keeps track of clicks for one, and secondly removes wood per click on the stoke button press
     global buttontime
     global wood
 
     currenttime = time.time()
-    if currenttime -buttontime >= 1:
+    if currenttime -buttontime >= 4:        #!!!
         print (time.time())
         buttontime = currenttime
         counter += 1
@@ -45,20 +46,20 @@ def woodclicker():
     if counter >= 3:
         message_label.place_forget()
 
-        
+                                                        #These are different thresholds that will make other buttons or messages appear.
     if counter >= 6:
         forestbutton.pack(side=LEFT, padx=10, pady=10)
 
     else:
         pass
 
-def getwoodclicker():
+def getwoodclicker():               #the getwood clicker funciton is for the first button in the forest area, it gets wood.
     global counter
     global buttontime
     global wood
 
     currenttime = time.time()
-    if currenttime -buttontime >= 5:
+    if currenttime -buttontime >= 9:        #!!!
         print (time.time())
         buttontime = currenttime
         counter += 1
@@ -68,7 +69,7 @@ def getwoodclicker():
         print(wood)
 
         if huts == 1:
-            wood += 25
+            wood += 25          #The amount of wood it will get per click is determined on huts, since I didn't know how to directly recreate them
 
         if huts == 2:
             wood +=40
@@ -80,16 +81,16 @@ def getwoodclicker():
 
 def maketrap():
     global wood
-    global traps
-    if wood >=30:
+    global traps            #This function will make a trap, which is how to currently get fur, teeth, and meat. 
+    if wood >=30:               #Bones would also be a good addition
         wood -= 30
         traps += 1
 
 def checktrap():
     global fur
     global teeth
-    global meat
-
+    global meat         #Check trap checks the trap. I tried to implement a timer like I did with the get wood and stoke functions, but I couldnt get it.
+                        #Everything has a random change to drop an amount within the range, and the amounts increase the more traps you have
 
     if traps == 1:
         fur += random.randint(1,7)
@@ -116,7 +117,7 @@ def checktrap():
 def makehut():
     global wood
     global huts
-    if wood >=100:
+    if wood >=100:      #This function makes a hut
         wood -=100
         huts +=1
 
@@ -130,8 +131,8 @@ def updatetextroom():
     button1.place(x=10, y=30)
 
     button2.place_forget()
-    if counter >= 9:
-        button2.config(text="make trap (-30 wood)")
+    if counter >= 9:                            #This funtion will essentially refresh the page each time it is called. It makes it forget what is what (as in values and content) and replaces it.
+        button2.config(text="make trap (-30 wood)") #Individually doing every single thing in the frames was pretty much the most effective way to do this, so as a result it is very long.
         button2.place(x= 10, y= 60)
 
     button3.place_forget()
@@ -159,7 +160,7 @@ def updatetextroom():
 
     if fur >= 1:
         rightframe_fur.place_forget()
-        rightframe_fur.config(text=f"fur: {fur}")
+        rightframe_fur.config(text=f"fur: {fur}")   #These if statements will make the materials appear on the right side frame when they actually aquire them, this purpose is to keep the user in the dark about what can be gotten.
         rightframe_fur.place(x=15, y=45)
 
     if meat >= 1:
@@ -181,10 +182,9 @@ def updatetextroom():
     
 
 def updatetextforest():
-    # message_label.pack_forget()
-    # message_label.config
+
     button1.place_forget()
-    button1.config(text="get wood", command=lambda: [getwoodclicker(), updatetextforest()])
+    button1.config(text="get wood", command=lambda: [getwoodclicker(), updatetextforest()]) #almost identical to the main one, except it refreshes the second area with different buttons
     button1.place(x=10, y=30)
 
     button2.place_forget()
@@ -192,10 +192,7 @@ def updatetextforest():
         button2.config(text="check trap", command= lambda: [checktrap(), updatetextforest()])
         button2.place(x= 10, y= 60)
 
-    button3.place_forget()
-    # if counter >= 15:
-        # button3.config(text="make hut (-100 wood)")
-        # button3.place(x=10, y=90)
+    button3.place_forget()          #The second area doesnt have a third button 
 
     leftframe_label.place_forget()
 
@@ -234,21 +231,16 @@ def updatetextforest():
         rightframe_huts.config(text=f"huts: {huts}")
         rightframe_huts.place(x=15, y=90)
 
-# def stokefire():
-#     global wood
-#     wood -= 1
-#     print(wood)
 
 #---------------------------------------------------------------------------------------------------------------
 #---------------------------------------------MAIN WINDOW AND FRAME---------------------------------------------
 #---------------------------------------------------------------------------------------------------------------
 
 window = Tk()
-window.title("Final Project")
-#window.geometry("500x700")
+window.title("Final Project")           #This is the main application window
 window.config(bg="gray")
 
-topframe = Frame(window, width=300, height=70)
+topframe = Frame(window, width=300, height=70)  #This is the top frame that has the buttons for the two areas, as seen below
 topframe.pack(padx=10, pady=10)
 topframe.pack_propagate(False)
 
@@ -256,7 +248,6 @@ roombutton = Button(topframe, text = "Dark Room", command=updatetextroom)
 roombutton.pack(side=LEFT, padx=10, pady=10)
 
 forestbutton = Button(topframe, text = "Desolate Forest", command=updatetextforest)
-#forestbutton.pack(side=LEFT, padx=10, pady=10)
 
 #---------------------------------------------------------------------------------------------------------------
 #---------------------------------------------FRAMES AND LABELS-------------------------------------------------
@@ -266,7 +257,7 @@ frame1 = Frame(window, width = 300, height = 200)
 frame1.pack(side=LEFT, padx=10, pady=10)
 frame1.pack_propagate(False)
 
-frame2 = Frame(window, width = 300, height = 200)
+frame2 = Frame(window, width = 300, height = 200)   #This is the left, middle, and right frames in order.
 frame2.pack(side=LEFT, padx=10, pady=10)
 frame2.pack_propagate(False)
 
@@ -278,7 +269,7 @@ frame3.pack_propagate(False)
 leftframe_label = Label(frame1, text="")
 
 rightframe_wood = Label(frame3, text="")
-rightframe_fur = Label(frame3, text="")
+rightframe_fur = Label(frame3, text="")         #These are the labels for the loot on the right side frame, that is achieved currently from traps only.
 rightframe_traps = Label(frame3, text="")
 rightframe_huts = Label(frame3, text="")
 rightframe_meat = Label(frame3, text="")
@@ -290,17 +281,15 @@ rightframe_teeth = Label(frame3, text="")
 
 message_label = Label(window, text="NOTE: Some buttons (like stoke, and get wood) have timers so the clicks don't count, but you can still click.", bg="gray", fg="white" )
 message_label.place(x= 200, y= 78)
-
+        # The above message label is a litte note between the top frame and mid frames. It is a disclaimer that an action performed for getting wood has a wait time, even thought the button is being pressed.
 
 button1 = Button(frame2, text="stoke", command=lambda: [woodclicker(),updatetextroom() ])
 button1.place(x=10, y=30)
 
-button2 = Button(frame2, text="make trap", command=lambda: [maketrap(), updatetextroom()])
-#button2.place(x= 10, y= 80)
+button2 = Button(frame2, text="make trap", command=lambda: [maketrap(), updatetextroom()])      #These are the main buttons. Only three at the moment. They are tied to their repsective funtions on the first area, 
+                                                                                                # WHen the second area refreshes it configs these buttons instead of making completely new ones.
 
 button3 = Button(frame2, text="make hut", command=lambda: [makehut(),updatetextroom() ])
-
-# button4 = Button(frame2, text="", command=lambda: [])
 
 #---------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------
